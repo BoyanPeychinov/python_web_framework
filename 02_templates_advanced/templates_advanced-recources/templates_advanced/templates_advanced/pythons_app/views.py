81959b717751a7af82549a0288ed718fb73a4ca8
BoyanPeychinov <BoyanPeychinov@gmail.com>
2021-07-17 20:02:43 +0300

    Templates advanced some stuff

--- 02_templates_advanced/templates_advanced-recources/templates_advanced/templates_advanced/pythons_app/views.py
+++ 02_templates_advanced/templates_advanced-recources/templates_advanced/templates_advanced/pythons_app/views.py
@@ -9,15 +9,15 @@ def index(req):
     return render(req, 'index.html', {'pythons': pythons})
 
 
-def create(req):
-    if req.method == 'GET':
+def create(request):
+    if request.method == 'GET':
         form = PythonCreateForm()
-        return render(req, 'create.html', {'form': form})
+        return render(request, 'create.html', {'form': form})
     else:
-        data = req.POST
-        form = PythonCreateForm(data)
+        form = PythonCreateForm(request.POST, request.FILES)
         print(form)
         if form.is_valid():
-            python = form.save()
-            python.save()
+            form.save()
             return redirect('index')
+
+        return render(request, 'create.html', {'form': form})
