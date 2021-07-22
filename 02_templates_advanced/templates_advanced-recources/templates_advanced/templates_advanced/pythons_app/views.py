d47b45b44027d32c08e3f3495c8b968d35dd2c12
BoyanPeychinov <BoyanPeychinov@gmail.com>
2021-07-22 19:07:43 +0300

    Authentication lab - create user, added sign up, sign in, sign out funcs, use of groups

--- 02_templates_advanced/templates_advanced-recources/templates_advanced/templates_advanced/pythons_app/views.py
+++ 02_templates_advanced/templates_advanced-recources/templates_advanced/templates_advanced/pythons_app/views.py
@@ -1,14 +1,31 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
+
 from .forms import PythonCreateForm
 from .models import Python
+from ..core.decorators import any_groups_required
 
 
-# Create your views here.
+def sign_in(request):
+    user = authenticate(username='Boyan', password='167943')
+    login(request, user)
+    return redirect('index')
+
+
+def sign_out(request):
+    logout(request)
+    return redirect('index')
+
+
 def index(req):
     pythons = Python.objects.all()
     return render(req, 'index.html', {'pythons': pythons})
 
 
+# @login_required(login_url=reverse_lazy('sign in'))
+@any_groups_required(groups=['User'])
 def create(request):
     if request.method == 'GET':
         form = PythonCreateForm()
