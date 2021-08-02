5e6e2c7aac9ab4f45a46da1f71c68a842193d720
BoyanPeychinov <BoyanPeychinov@gmail.com>
2021-08-02 14:58:41 +0300

    Using class based views, user extension with profile, some mixin and other stuff

--- pythons/pythons/pythons_app/views.py
+++ pythons/pythons/pythons_app/views.py
@@ -1,29 +1,46 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from .forms import PythonCreateForm
 from .models import Python
 from ..core.decorators import any_groups_required
 
 
-def index(req):
-    pythons = Python.objects.all()
-    return render(req, 'index.html', {'pythons': pythons})
+# def index(req):
+#     pythons = Python.objects.all()
+#     return render(req, 'index.html', {'pythons': pythons})
+from ..core.mixins import AnyGroupRequiredMixin
+
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    model = Python
+    context_object_name = 'pythons'
+    paginate_by = 5
 
 
 # @login_required(login_url=reverse_lazy('sign in'))
 # @any_groups_required(groups=['User'])
-def create(request):
-    if request.method == 'GET':
-        form = PythonCreateForm()
-        return render(request, 'create.html', {'form': form})
-    else:
-        form = PythonCreateForm(request.POST, request.FILES)
-        print(form)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
+# def create(request):
+#     if request.method == 'GET':
+#         form = PythonCreateForm()
+#         return render(request, 'create.html', {'form': form})
+#     else:
+#         form = PythonCreateForm(request.POST, request.FILES)
+#         print(form)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('index')
+#
+#         return render(request, 'create.html', {'form': form})
 
-        return render(request, 'create.html', {'form': form})
+
+class PythonCreateView(AnyGroupRequiredMixin, CreateView):
+    template_name = 'create.html'
+    model = Python
+    fields = '__all__'
+    success_url = reverse_lazy('index')
--- pythons/pythons/pythons_auth/urls.py
+++ pythons/pythons/pythons_auth/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from pythons.pythons_auth import views
+from pythons.pythons_auth.views import SignUpView, SignInView
 
 urlpatterns = [
-    path('sign-up/', views.sign_up, name='sign up'),
-    path('sign-in/', views.sign_in, name='sign in'),
+    # path('sign-up/', views.sign_up, name='sign up'),
+    path('sign-up/', SignUpView.as_view(), name='sign up'),
+    # path('sign-in/', views.sign_in, name='sign in'),
+    path('sign-in/', SignInView.as_view(), name='sign in'),
     path('sign-out/', views.sign_out, name='sign out'),
 ]
